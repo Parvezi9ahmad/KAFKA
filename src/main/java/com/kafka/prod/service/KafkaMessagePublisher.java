62d1815d5e952bfec8f0b9b2d3efec2efa47fd95
parvez Ahmad <parvezi9ahmad@gmail.com>
2026-02-11 18:02:46 +0530

    saving data

---
+++ src/main/java/com/kafka/prod/service/KafkaMessagePublisher.java
@@ -0,0 +1,27 @@
+package com.kafka.prod.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessagePublisher {
+
+	@Autowired
+	private KafkaTemplate<String, Object> template;
+
+	public void sendMessageToTopic(String message) {
+		CompletableFuture<SendResult<String, Object>> future = template.send("javatechidemo-1", message);
+		future.whenComplete((result, ex) -> {
+			if (ex == null) {
+				System.out.println(
+						"SENDING MESSAGE :[" + message + "] OFFSET DATA:" + result.getRecordMetadata().offset());
+			} else {
+				System.out.println("Unable to send the message " + message + " due to" + ex.getMessage());
+			}
+		});
+	}
+}
