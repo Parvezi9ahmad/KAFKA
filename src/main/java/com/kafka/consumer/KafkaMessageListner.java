c0acbf540720288b4b0a3be5fe366778fc6a3713
parvez Ahmad <parvezi9ahmad@gmail.com>
2026-02-12 14:04:16 +0530

    json serialization and deserialization

---
+++ src/main/java/com/kafka/consumer/KafkaMessageListner.java
@@ -0,0 +1,33 @@
+package com.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.kafka.dto.Customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class KafkaMessageListner {
+	private static final Logger log = LoggerFactory.getLogger(KafkaMessageListner.class);
+
+	@KafkaListener(topics = "javatechidemo10",groupId = "test-group")
+	public void consume(Customer customer) {
+		log.info("consume cosumes the message {}", customer.toString());
+	}
+	
+	/*
+	 * @KafkaListener(topics = "javatechidemo-1",groupId = "jt-group") public void
+	 * consume2(String message) { log.info("consumer2 cosumes the message {}",
+	 * message); }
+	 * 
+	 * @KafkaListener(topics = "javatechidemo-1",groupId = "jt-group") public void
+	 * consume3(String message) { log.info("consumer3 cosumes the message {}",
+	 * message); }
+	 * 
+	 * @KafkaListener(topics = "javatechidemo-1",groupId = "jt-group") public void
+	 * consume4(String message) { log.info("consumer4 cosumes the message {}",
+	 * message); }
+	 */
+}
