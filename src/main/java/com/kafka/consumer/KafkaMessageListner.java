565c4f10299c9657cbf715345324a2792d859804
parvez Ahmad <parvezi9ahmad@gmail.com>
2026-02-12 16:30:00 +0530

    topic partition in consumer

--- src/main/java/com/kafka/consumer/KafkaMessageListner.java
+++ src/main/java/com/kafka/consumer/KafkaMessageListner.java
@@ -7,16 +7,22 @@ import com.kafka.dto.Customer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.kafka.annotation.TopicPartition;
 @Service
 public class KafkaMessageListner {
 	private static final Logger log = LoggerFactory.getLogger(KafkaMessageListner.class);
 
-	@KafkaListener(topics = "javatechidemo10",groupId = "test-group")
-	public void consume(Customer customer) {
+//	@KafkaListener(topics = "javatechidemo10",groupId = "test-group")
+//	public void consume(Customer customer) {
+//		log.info("consume cosumes the message {}", customer.toString());
+//	}
+	
+	@KafkaListener(groupId = "test-group",topicPartitions = {
+	        @TopicPartition(topic = "pt-topic", partitions = {"3"})
+	    })
+	public void consume(String customer) {
 		log.info("consume cosumes the message {}", customer.toString());
 	}
-	
 	/*
 	 * @KafkaListener(topics = "javatechidemo-1",groupId = "jt-group") public void
 	 * consume2(String message) { log.info("consumer2 cosumes the message {}",
